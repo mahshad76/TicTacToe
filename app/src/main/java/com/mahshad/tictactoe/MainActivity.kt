c012e4498757f03f4be5fe6ef77b7f5f6845adcc
mahshad76 <mahshadmahdavimoghadam@gmail.com>
2025-11-12 16:38:33 -0500

    the simple graphic board is designed

--- app/src/main/java/com/mahshad/tictactoe/MainActivity.kt
+++ app/src/main/java/com/mahshad/tictactoe/MainActivity.kt
@@ -4,13 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.mahshad.tictactoe.ui.HomeBoardGame
 import com.mahshad.tictactoe.ui.theme.TicTacToeTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +13,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TicTacToeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                HomeBoardGame()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TicTacToeTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/mahshad/tictactoe/ui/HomeBoardGame.kt
@@ -0,0 +1,46 @@
+package com.mahshad.tictactoe.ui
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun HomeBoardGame(modifier: Modifier = Modifier) {
+    Card() {
+        repeat(3) {
+            Row() {
+                repeat(3) {
+                    Text(
+                        "click",
+                        textAlign = TextAlign.Center,
+                        modifier = modifier
+                            .size(50.dp)
+                            .padding(8.dp)
+                            .background(Color.Gray)
+                            .clickable(
+                                enabled = true,
+                                onClickLabel = "",
+                                onClick = { }
+                            )
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+@Preview
+fun Preview() {
+    HomeBoardGame()
+}
